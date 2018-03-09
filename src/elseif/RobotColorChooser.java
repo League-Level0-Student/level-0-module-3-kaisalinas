a0ba9dd45f81a5fbfce8fc00b2b9cc363cc428b7
Kai Salinas <league@WTS14.local>
2018-03-08 19:58:37 -0800

    module 3 stuff

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,39 @@
 package elseif;
 
 import javax.swing.JOptionPane;
-import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		Robot r2d2 = new Robot();
+		for (int i = 0; i < 10; i++) {
+			// 1. Create a new Robot
 
-        //6. If the user doesn’t enter anything, choose a random color
+			// 3. Ask the user what color they would like the robot to draw
+			String a = JOptionPane.showInputDialog("What color do you want the robot to draw");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (a.equals("blue")) {
+				r2d2.setPenColor(0, 0, 255);
+			} else {
+				r2d2.setRandomPenColor();
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
+			r2d2.setRandomPenColor();
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+			// 4. Set the pen width to 10
+			r2d2.setPenWidth(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			r2d2.penDown();
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+		}
 	}
 }
